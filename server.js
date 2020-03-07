b0f232785faa948c4048d631a8fa4a18788db7bb
James Calverley <james.calverley3@gmail.com>
2020-03-07 15:28:03 -0500

    adds v2 folder

--- server.js
+++ server.js
@@ -1,28 +1,60 @@
+
+//? THINGS TO DO ==================
+//* COMPLETE add multiple forms 
+//* COMPLETE add forms and list to one page
+//* COMPLETE display all form data
+//* COMPLATE create onpage load and render functions
+//* COMPLETE pass all form data to array
+//* COMPLETE create render function that displays on page load, as well as on submit
+// create database in mysql
+// create orm file that writes data to a database
+
+//? additonal =============
+// add delete button
+// add to Heruko
+
+
 const express = require('express');
 
 const app = express();
-const PORT = process.env || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('html'));
 
+const personData = [
+    { 
+        firstName: "James",
+        lastName: "Calverley",
+        location: "Kettleby"
+    },
+    {
+        firstName: "Claude",
+        lastName: "Claudey-Boi",
+        location: "Flesherton"  
+    },
+    {
+        firstName: "Barrack",
+        lastName: "Obama",
+        location: "Washington"  
+    }
 
-const foods = [
-    { name: "pizza" }, { name: "sushi"}, { name: "ice cream"}
 ];
 
-app.get('/api/foodlist', function( req, res ){
-    res.send(foods);
+app.get('/api/person-data-list', function( req, res ){
+    res.send(personData);
 });
 
-app.post('/api/foodlist', function( req, res){
+app.post('/api/person-data', function( req, res){
     console.log(req.body);
-    foods.push(req.body);
-    res.send(foods);
+    personData.push(req.body);
+    console.log(personData)
+    res.send(personData);
 })
 
 app.listen(PORT, function(){
     console.log("Server is runnning...PORT: " + PORT)
 });
+
